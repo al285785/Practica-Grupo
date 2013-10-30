d528472769a8bc51b3f48ea1b52d0f8b1b0b8cfb
YaminoTenshi <angelillotorres@hotmail.com>
2013-10-30 16:03:56 +0100

    cambios en mi rama

--- BinarioDecimal.py
+++ BinarioDecimal.py
@@ -4,4 +4,14 @@ def Decimal2Binario(decimal):
         binario = str(decimal % 2) + binario
         decimal = decimal // 2
     return str(decimal) + binario
+
+def Binario2Decimal(binario):
+    n=len(binario)
+    valor=0
+    for bit in binario:
+         if bit == '1':
+              valor=valor+2**(n-1)
+         n -=1
+    return valor
+
  
